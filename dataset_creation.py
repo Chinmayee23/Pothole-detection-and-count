6e026a3aec2246e13a27376dacffaafcbae107b4
Chinmayee23 <40840102+Chinmayee23@users.noreply.github.com>
2018-11-08 21:40:43 +0530

    Add files via upload

---
+++ dataset_creation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2 as cv
+import scipy.misc, scipy.ndimage
+
+img = cv.imread('E:/pothole-detection-source-code-master/test6.png',0)
+edge = cv.Canny(img,100,200)     
+
+A=np.reshape(edge, (edge.size,-1))
+np.savetxt("t6.csv", A, delimiter=",")
